03e035b292ef598143b69d4e9da491e11c020391
Matin Tamizi <matin@cuenca.com>
2020-05-13 16:47:10 -0500

    api should _always_ return the network

--- cuenca/resources/transfers.py
+++ cuenca/resources/transfers.py
@@ -32,7 +32,7 @@ class Transfer(Creatable, Queryable, Retrievable):
     descriptor: str  # how it'll appear for the recipient
     idempotency_key: str
     status: Status
-    network: Optional[Network] = None
+    network: Network
     tracking_key: Optional[str] = None  # clave rastreo
 
     @classmethod
