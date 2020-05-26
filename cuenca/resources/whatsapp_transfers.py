012f7e6effc57740ce4cc95a46239e1c8fb1be3f
Matin Tamizi <matin@cuenca.com>
2020-05-26 14:55:04 -0500

    add more fields to WhatsappTransfer

--- cuenca/resources/whatsapp_transfers.py
+++ cuenca/resources/whatsapp_transfers.py
@@ -4,6 +4,7 @@ from typing import ClassVar, Optional
 
 from pydantic.dataclasses import dataclass
 
+from ..types import TransferNetwork
 from .accounts import Account
 from .base import Transaction
 from .resources import retrieve_uri
@@ -13,10 +14,14 @@ from .resources import retrieve_uri
 class WhatsappTransfer(Transaction):
     _resource: ClassVar = 'whatsapp_transfers'
 
+    recipient_name: str
+    phone_number: str
     claim_url: str
     expires_at: dt.datetime
     # defined after the transfer has been claimed
     destination_uri: Optional[str]
+    network: TransferNetwork
+    tracking_key: Optional[str] = None  # clave rastreo if network is SPEI
 
     @property  # type: ignore
     @lru_cache()
