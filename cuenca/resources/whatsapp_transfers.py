571a4ad745be043a3d519ea237d8d5f906901474
Matin Tamizi <matin@cuenca.com>
2020-05-26 12:48:05 -0500

    WhatsappTransfer.destination

--- cuenca/resources/whatsapp_transfers.py
+++ cuenca/resources/whatsapp_transfers.py
@@ -1,14 +1,31 @@
 import datetime as dt
-from typing import ClassVar
+from functools import lru_cache
+from typing import ClassVar, Optional
 
 from pydantic.dataclasses import dataclass
 
+from .accounts import Account
 from .base import Transaction
+from .resources import retrieve_uri
 
 
 @dataclass
 class WhatsappTransfer(Transaction):
     _resource: ClassVar = 'whatsapp_transfers'
 
-    shared_url: str
+    claim_url: str
     expires_at: dt.datetime
+    # defined after the transfer has been claimed
+    destination_uri: Optional[str]
+
+    @property  # type: ignore
+    @lru_cache()
+    def destination(self) -> Optional[Account]:
+        if self.destination_uri is None:
+            dest = None
+        else:
+            dest = retrieve_uri(self.destination_uri)
+        return dest
+
+    def __hash__(self):
+        return hash((self._resource, self.id, self.destination_uri))
