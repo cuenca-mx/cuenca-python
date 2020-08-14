f90734144309bc87ee0d6232429f7897a248ca43
Matin Tamizi <matin@users.noreply.github.com>
2020-08-14 17:19:13 -0500

    give preference to basic auth

--- cuenca/http/client.py
+++ cuenca/http/client.py
@@ -56,7 +56,7 @@ class Session:
 
     @property
     def auth(self) -> Union[AWSRequestsAuth, Tuple[str, str]]:
-        return self.iam_auth or self.basic_auth  # give preference to IAM auth
+        return self.basic_auth or self.iam_auth  # give preference to basic auth
 
     def configure(
         self,
