1f3f2ad9bfd4edba68850c6614cf489566839371
Felipe López <flh.1989@gmail.com>
2020-08-17 15:28:31 -0500

    tests

--- cuenca/http/client.py
+++ cuenca/http/client.py
@@ -15,7 +15,7 @@ from ..exc import CuencaResponseException
 from ..version import API_VERSION, CLIENT_VERSION
 
 API_HOST = 'api.cuenca.com'
-SANDBOX_HOST = 'sandbox.cuenca.com'
+SANDBOX_HOST = 'bxanq6vtyf.execute-api.us-east-1.amazonaws.com/api'
 AWS_DEFAULT_REGION = 'us-east-1'
 AWS_SERVICE = 'execute-api'
 
