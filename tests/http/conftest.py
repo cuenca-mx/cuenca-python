49c20980d84da4b3d9a893bb02e0b390531921d4
Felipe López <flh.1989@gmail.com>
2020-08-17 16:33:36 -0500

    remove unused params

--- tests/http/conftest.py
+++ tests/http/conftest.py
@@ -11,3 +11,4 @@ def cuenca_creds(monkeypatch) -> None:
 def aws_creds(monkeypatch) -> None:
     monkeypatch.setenv('AWS_ACCESS_KEY_ID', 'aws_key')
     monkeypatch.setenv('AWS_SECRET_ACCESS_KEY', 'aws_secret')
+    monkeypatch.setenv('AWS_DEFAULT_REGION', 'us-east-1')
