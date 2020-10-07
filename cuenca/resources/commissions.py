bb572fe7c81628b0391418eebebd05cfbcd994b6
keryc Diaz <kerycdiaz@gmail.com>
2020-10-07 12:13:33 -0500

    fix relatedtransaction

--- cuenca/resources/commissions.py
+++ cuenca/resources/commissions.py
@@ -1,6 +1,7 @@
+import re
 from typing import ClassVar, Optional, cast
 
-from cuenca_validations.types import EntryModel, LedgerEntryType
+from cuenca_validations.types import EntryModel, EntryType
 from pydantic.dataclasses import dataclass
 
 from cuenca import resources
@@ -9,19 +10,28 @@ from .base import Transaction
 from .resources import retrieve_uri
 
 
+class RelatedTransaction(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, rt_uri):
+        rt_match = re.search(r'/[a-z]+/(\w+)', rt_uri)
+        rt_id = rt_match.group(1)
+        entry = EntryModel(id=rt_id, type=EntryType.commission)
+        return rt_uri, entry.get_model()
+
+
 @dataclass
 class Commission(Transaction):
     _resource: ClassVar = 'commissions'
 
-    related_transaction_uri: Optional[str]
+    related_transaction_uri: Optional[RelatedTransaction]
 
     @property  # type: ignore
     def related_transaction(self):
         if not self.related_transaction_uri:
             return None
-        related_transaction = self.related_transaction_uri.split('/')[-1]
-        entry = EntryModel(id=related_transaction, type=LedgerEntryType.credit)
-        return cast(
-            getattr(resources, entry.get_model()),
-            retrieve_uri(self.related_transaction_uri),
-        )
+        rt_uri, entry_model = self.related_transaction_uri
+        return cast(getattr(resources, entry_model), retrieve_uri(rt_uri))
