8885032f518f20566d4f4bccd32db8e4d52a6f99
Keryc Diaz <keryc@cuenca.com>
2020-10-13 18:10:17 -0500

    Commissions with related transactions (#65)
    * tests comissions
    
    * fix test
    
    * fix entry model
    
    * fix relatedtransaction
    
    * commission end
    
    * change version valid
    
    * fix entry model
    
    * fix Commission
    
    * use mapper in commission
    
    * update version
    
    * fix end
    
    * change id
    
    * rebase and change version cuenca-validations

--- cuenca/resources/commissions.py
+++ cuenca/resources/commissions.py
@@ -1,12 +1,31 @@
-from typing import ClassVar, Optional
+from typing import ClassVar, Optional, cast
 
+from cuenca_validations.types import (
+    CommissionType,
+    EntryType,
+    RelatedTransaction,
+)
 from pydantic.dataclasses import dataclass
 
+from cuenca import resources
+
 from .base import Transaction
+from .resources import retrieve_uri
+
+mapper = {CommissionType.cash_deposit: EntryType.credit}
 
 
 @dataclass
 class Commission(Transaction):
     _resource: ClassVar = 'commissions'
 
-    related_transaction_uri: Optional[str]
+    type: CommissionType
+    related_transaction_uri: Optional[RelatedTransaction]
+
+    @property  # type: ignore
+    def related_transaction(self):
+        rt = self.related_transaction_uri
+        if not rt:
+            return None
+        resource = getattr(resources, rt.get_model())
+        return cast(resource, retrieve_uri(rt)) if resource else None
