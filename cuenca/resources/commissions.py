10b72a38b8d488a3bba6c1e7b90f30c978b78fe3
keryc Diaz <kerycdiaz@gmail.com>
2020-10-13 11:33:37 -0500

    fix Commission

--- cuenca/resources/commissions.py
+++ cuenca/resources/commissions.py
@@ -1,6 +1,10 @@
 from typing import ClassVar, Optional, cast
 
-from cuenca_validations.types import CommissionType, RelatedTransaction
+from cuenca_validations.types import (
+    CommissionType,
+    EntryType,
+    RelatedTransaction,
+)
 from pydantic.dataclasses import dataclass
 
 from cuenca import resources
@@ -21,5 +25,13 @@ class Commission(Transaction):
         rt = self.related_transaction_uri
         if not rt:
             return None
-        model = rt.get_model(self.type.value)
-        return cast(getattr(resources, model), retrieve_uri(rt))
+        model = (
+            rt.get_model(EntryType.credit.value)
+            if self.type == CommissionType.cash_deposit
+            else None
+        )
+        return (
+            cast(getattr(resources, model), retrieve_uri(rt))
+            if model
+            else None
+        )
