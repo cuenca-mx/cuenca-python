bfdad3d29f4691d207a084f14a7fac0a6178132d
Yisus <alan@cuenca.com>
2020-10-19 11:59:06 -0500

    add type

--- cuenca/resources/balance_entries.py
+++ cuenca/resources/balance_entries.py
@@ -1,6 +1,9 @@
-from typing import ClassVar
+from typing import ClassVar, cast
 
+from cuenca_validations.types import EntryType, RelatedTransaction
 from pydantic.dataclasses import dataclass
+
+from cuenca import resources
 
 from .base import Queryable, Retrievable
 from .resources import retrieve_uri
@@ -15,7 +18,17 @@ class BalanceEntry(Retrievable, Queryable):
     name: str
     rolling_balance: int
     transaction_uri: str
+    type: EntryType
+    related_transaction_uri: RelatedTransaction
 
     @property  # type: ignore
     def transaction(self):
         return retrieve_uri(self.transaction_uri)
+
+    @property  # type: ignore
+    def related_transaction(self):
+        rt = self.related_transaction_uri
+        if not rt:
+            return None
+        resource = getattr(resources, rt.get_model())
+        return cast(resource, retrieve_uri(rt)) if resource else None
