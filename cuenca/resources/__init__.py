7ac8be6ecadcacffdd2c3d80fbb81546748cebe0
keryc Diaz <kerycdiaz@gmail.com>
2020-10-21 16:12:18 -0500

    fix init CardTransaction

--- cuenca/resources/__init__.py
+++ cuenca/resources/__init__.py
@@ -16,6 +16,7 @@ from .accounts import Account
 from .api_keys import ApiKey
 from .balance_entries import BalanceEntry
 from .bill_payments import BillPayment
+from .card_transactions import CardTransaction
 from .cards import Card
 from .commissions import Commission
 from .deposits import Deposit
