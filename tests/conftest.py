3b5bfc9eed56aeefdafaae7bdc9076b6bcfe7b4e
cloud-init created default user <ec2-user@ip-10-0-170-199.us-east-2.compute.internal>
2020-10-21 18:50:23 +0000

    Cassettes

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,5 +8,5 @@ cuenca.configure(sandbox=True)
 @pytest.fixture(scope='module')
 def vcr_config():
     config = dict()
-    config['filter_headers'] = [('Authorization', 'DUMMY')]
+    config['filter_headers'] = [('Authorization', 'DUMMY'), ('X-Cuenca-Token', 'DUMMY')]
     return config
