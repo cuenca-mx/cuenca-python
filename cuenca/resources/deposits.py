ec773fed074f7434e8743a07fe9300f3c3a31da1
keryc Diaz <kerycdiaz@gmail.com>
2020-10-29 17:34:13 -0600

    fix end

--- cuenca/resources/deposits.py
+++ cuenca/resources/deposits.py
@@ -1,7 +1,7 @@
-from pydantic.dataclasses import dataclass
 from typing import ClassVar, Optional, cast
 
 from cuenca_validations.types import DepositNetwork, DepositQuery
+from pydantic.dataclasses import dataclass
 
 from .accounts import Account
 from .base import Transaction
