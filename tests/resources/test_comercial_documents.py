58e88039ee868b3e250c91dc4447960ee47667e5
keryc Diaz <kerycdiaz@gmail.com>
2020-12-14 12:38:16 -0600

    tests end

--- tests/resources/test_comercial_documents.py
+++ tests/resources/test_comercial_documents.py
@@ -1,8 +1,10 @@
-from datetime import datetime
+import os
+from datetime import date
 
 import pytest
 from cuenca_validations.types import DocumentType
 from cuenca_validations.types.requests import DocumentRequest
+from pydantic import ValidationError
 
 from cuenca import ComercialDocument
 
@@ -12,12 +14,44 @@ def test_comercial_documents_create():
     document = ComercialDocument.create(
         DocumentRequest(
             client_name='Iron Man',
-            clabe='32521352365236236',
+            clabe='002000000000000008',
             address='Address Foo',
-            rfc='RFCXXXXXXXXX',
-            date=datetime(2020, 11, 1),
+            rfc='GODE561231GR8',
+            date=(2020, 11),
             document_type=DocumentType.invoice,
         )
     )
     assert document.id is not None
     assert document.body is not None
+
+    zip_name = 'CDceVNfKmzRH6PDgFnlHwgsg.zip'
+    base_dir = os.path.abspath(os.getcwd())
+
+    document.download(path=f'{base_dir}/tests/resources')
+
+    assert os.path.exists(f'{base_dir}/tests/resources/{zip_name}')
+
+
+def test_comercial_documents_create_errors():
+    now = date.today()
+    with pytest.raises(ValidationError) as exc_info:
+        ComercialDocument.create(
+            DocumentRequest(
+                client_name='Iron Man',
+                clabe='002000000000000008',
+                address='Address Foo',
+                rfc='fooo',
+                date=(now.year, now.month),
+                document_type=DocumentType.invoice,
+            )
+        )
+    assert exc_info.value.errors()[0] == dict(
+        loc=('rfc',),
+        type='value_error',
+        msg='Invalid rfc format',
+    )
+    assert exc_info.value.errors()[1] == dict(
+        loc=('date',),
+        type='value_error',
+        msg='You cannot check the current month',
+    )
