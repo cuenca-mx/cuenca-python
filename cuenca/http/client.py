f582db224b3a0ea9faec26caf3790966dcf602fd
Felipe López <flh.1989@gmail.com>
2021-02-11 14:19:05 -0600

    cassettes

--- cuenca/http/client.py
+++ cuenca/http/client.py
@@ -16,7 +16,7 @@ from ..jwt import Jwt
 from ..version import API_VERSION, CLIENT_VERSION
 
 API_HOST = 'api.cuenca.com'
-SANDBOX_HOST = 'stage.cuenca.com'
+SANDBOX_HOST = 'sandbox.cuenca.com'
 AWS_DEFAULT_REGION = 'us-east-1'
 AWS_SERVICE = 'execute-api'
 
