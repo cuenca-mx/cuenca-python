ad571f88d878348c8a940424779d93eb6e28e5f1
Ricardo Sánchez <rsc@cuenca.com>
2021-02-23 13:42:09 -0600

    Cassettes for everyone

---
+++ tests/resources/test_card_activations.py
@@ -0,0 +1,37 @@
+import pytest
+from cuenca_validations.types import CardFundingType, CardIssuer, CardStatus
+
+from cuenca.exc import CuencaResponseException
+from cuenca.resources import CardActivation
+
+
+@pytest.mark.vcr
+def test_card_activation():
+    values = dict(
+        number='4122943400145596',
+        exp_month=11,
+        exp_year=24,
+        cvv2='123',
+        issuer=CardIssuer.accendo,
+        funding_type=CardFundingType.debit,
+    )
+    card = CardActivation.create(**values)
+    assert all(getattr(card, key) == value for key, value in values.items())
+    assert card.user_id == 'US1237'
+    assert card.status is CardStatus.active
+
+
+@pytest.mark.vcr
+def test_blocked_attemps():
+    values = dict(
+        number='4122943499999999',
+        exp_month=11,
+        exp_year=24,
+        cvv2='123',
+        issuer=CardIssuer.accendo,
+        funding_type=CardFundingType.debit,
+    )
+    for _ in range(7):
+        with pytest.raises(CuencaResponseException) as e:
+            CardActivation.create(**values)
+    assert 'ForbiddenError: Too many attempts' == e.value.json['Message']
