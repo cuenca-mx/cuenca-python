16be87b0685586c52c085af3dc1d95e80e6ab7ec
rogelioLpz <rogelio.lpz94@gmail.com>
2021-03-30 11:01:42 -0600

    tests

---
+++ tests/resources/test_card_validations.py
@@ -0,0 +1,22 @@
+from cuenca.resources import CardValidation
+
+
+def test_card_validations():
+    card_data = dict(
+        number='5448750129965637',
+        cvv='150',
+        cvv2='685',
+        icvv='399',
+        exp_month=2,
+        exp_year=25,
+        pin_block='B57D25D49FD1F88A',
+    )
+    validation = CardValidation.create(**card_data)
+    assert validation.is_active
+    assert validation.card_uri is not None
+    assert validation.is_valid_cvv
+    assert validation.is_valid_cvv2
+    assert validation.is_valid_icvv
+    assert validation.is_valid_pin_block
+    assert validation.is_valid_exp_date
+    assert not validation.is_expired
