7e817e729a98b3a788e022021fbbfdc071fd17fb
rogelioLpz <rogelio.lpz94@gmail.com>
2021-04-01 18:13:09 -0600

    card_id to number

--- tests/resources/test_arpc.py
+++ tests/resources/test_arpc.py
@@ -6,11 +6,11 @@ from cuenca.resources import ARPC
 @pytest.mark.skip(reason="Not ready")
 def test_arpc():
     arpc_req = dict(
+        number='1234567890123403',
         arqc='test_arqc',
         key_derivation_method='3',
         arpc_method='1',
         txn_data='justsomerandomdata',
-        card_id='CA1234',
         arc='arc',
         pan_sequence='00',
         unique_number='number',
