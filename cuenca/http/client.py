d0f6b44d01dc4db1ce1ebc59e98c7035793aa86d
rogelioLpz <rogelio.lpz94@gmail.com>
2021-08-13 15:54:49 -0500

    serialize datetime

--- cuenca/http/client.py
+++ cuenca/http/client.py
@@ -21,11 +21,6 @@ API_HOST = 'api.cuenca.com'
 SANDBOX_HOST = 'sandbox.cuenca.com'
 
 
-def json_serializer(o):
-    if isinstance(o, (dt.date, dt.datetime)):
-        return o.isoformat()
-
-
 class Session:
 
     host: str = API_HOST
@@ -110,12 +105,17 @@ class Session:
             if self.jwt_token.is_expired:
                 self.jwt_token = Jwt.create(self)
             self.session.headers['X-Cuenca-Token'] = self.jwt_token.token
-        json_data = json.loads(json.dumps(data, default=json_serializer))
+
+        if data is not None:
+            for key, value in data.items():
+                if isinstance(value, (dt.date, dt.datetime)):
+                    data[key] = value.isoformat()
+
         resp = self.session.request(
             method=method,
             url='https://' + self.host + urljoin('/', endpoint),
             auth=self.auth,
-            json=json_data,
+            json=data,
             params=params,
             **kwargs,
         )
