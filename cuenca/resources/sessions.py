286c54c0bed0d37d5295d2f8286f1496c8ba45c7
Manuel Avalos <manuelavalos97@gmail.com>
2022-01-27 15:37:14 -0600

    Session model

---
+++ cuenca/resources/sessions.py
@@ -0,0 +1,23 @@
+import datetime as dt
+from typing import ClassVar, Optional
+
+from cuenca_validations.types import EventType
+from pydantic.dataclasses import dataclass
+
+from .base import Creatable, Retrievable
+
+
+@dataclass
+class Session(Creatable, Retrievable):
+    _resource: ClassVar = 'sessions'
+
+    id: str
+    created_at: dt.datetime
+    updated_at: dt.datetime
+    user_id: str
+    platform_uri: str
+    expires_at: dt.datetime
+    policy_uri: str
+    redirect_url: Optional[str]
+    cancel_url: Optional[str]
+    event_type: Optional[EventType]
