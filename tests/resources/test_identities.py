c8d8f1f49c24d7449d92e0e22dcf4d2c6d9c3960
Manuel Avalos <manuelavalos97@gmail.com>
2022-01-31 15:59:14 -0600

    Fixing tests

--- tests/resources/test_identities.py
+++ tests/resources/test_identities.py
@@ -4,9 +4,7 @@ from cuenca.resources import CurpValidation, Identity
 
 
 @pytest.mark.vcr
-@pytest.mark.skip
 def test_create_identity(curp_validation_request):
-    # https://sentry.io/organizations/cuenca-mx/issues/2971297839/?project=6118571&query=is%3Aunresolved
     # creating a curp_validation automatically creates the identity
     curp_validation = CurpValidation.create(curp_validation_request)
     assert curp_validation.renapo_curp_match
