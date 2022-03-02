a50679488ee8b5415e40df186c051c89c7d290d6
Yisus <alan@cuenca.com>
2022-03-02 12:44:39 -0600

    just for testing

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
     python_requires='>=3.6',
     install_requires=[
         'requests>=2.24,<28',
-        'cuenca-validations>=0.9.18,<0.9.19',
         'dataclasses>=0.7;python_version<"3.7"',
     ],
     classifiers=[
