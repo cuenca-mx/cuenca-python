8eee22d69b31676fc3b1310dbee99702ae4c94f9
Luis Brime <luisbrimeg@gmail.com>
2022-03-03 11:56:25 -0600

    fixed tests

--- cuenca/resources/users.py
+++ cuenca/resources/users.py
@@ -43,7 +43,7 @@ class User(Creatable, Retrievable, Updateable, Queryable):
     proof_of_address: Optional[KYCFile]
     proof_of_life: Optional[KYCFile]
     beneficiaries: Optional[List[Beneficiary]]
-    clabe: Optional[str]
+    clabe: Optional[str] = None
     platform_id: Optional[str] = None
 
     @classmethod
