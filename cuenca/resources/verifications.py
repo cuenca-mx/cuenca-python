558ec003af2bf84901c40a965fb46400717b58ef
Andres Hernandez <andres@cuenca.com>
2022-03-11 19:01:19 -0600

    First Approach of Verifications

---
+++ cuenca/resources/verifications.py
@@ -0,0 +1,46 @@
+import datetime as dt
+from typing import ClassVar, Optional, Union, cast
+
+from cuenca_validations.types import VerificationType
+from cuenca_validations.types.identities import PhoneNumber
+from cuenca_validations.types.requests import VerificationRequest, VerificationAttemptRequest
+from pydantic import EmailStr
+from pydantic.dataclasses import dataclass
+
+from ..http import Session, session as global_session
+from .base import Creatable, Verifiable
+
+
+@dataclass
+class Verification(Creatable, Verifiable):
+    _resource: ClassVar = 'verifications'
+
+    sender: Union[EmailStr, PhoneNumber]
+    type: VerificationType
+    created_at: dt.datetime
+    updated_at: Optional[dt.datetime]
+    deactivated_at: Optional[dt.datetime]
+
+
+    @classmethod
+    def create(
+        cls,
+        sender: str,
+        type: VerificationType,
+        *,
+        session: Session = global_session
+    ) -> 'Verification':
+        req = VerificationRequest(sender=sender, type=type)
+        return cast(
+            'Verification', cls._create(**req.dict(), session=session)
+        )
+
+
+    @classmethod
+    def verify(
+        cls, id: str, code: str, *, session: Session = global_session,
+    ) -> 'Verification':
+        req = VerificationAttemptRequest(code=code)
+        return cast(
+            'Verification', cls._update(id=id, **req.dict(), session=session),
+        )
