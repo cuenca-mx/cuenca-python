35c41af9032aef6eba7e2f99e5898176473341ff
Andres Hernandez <andres@cuenca.com>
2022-03-29 14:19:39 -0600

    CV version and chaanging tests

--- tests/resources/test_verifications.py
+++ tests/resources/test_verifications.py
@@ -9,7 +9,7 @@ from cuenca.exc import CuencaResponseException
 def test_verification_email_create():
     verification: Verification = Verification.create(
         recipient='mail@cuenca.com',
-        type=VerificationType.email_verification,
+        type=VerificationType.email,
         platform_id='PL01',
     )
     assert verification.id
@@ -19,7 +19,7 @@ def test_verification_email_create():
 def test_verification_phone_create():
     verification: Verification = Verification.create(
         recipient='+525555555555',
-        type=VerificationType.phone_verification,
+        type=VerificationType.phone,
         platform_id='PL01',
     )
     assert verification.id
