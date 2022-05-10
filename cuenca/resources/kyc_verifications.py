5a349c6e141de6093283c4b7c4a8931fada6c21d
Yisus <alan@cuenca.com>
2022-05-10 12:56:35 -0500

    Update kyc curp

--- cuenca/resources/kyc_verifications.py
+++ cuenca/resources/kyc_verifications.py
@@ -1,15 +1,15 @@
 import datetime as dt
 from typing import ClassVar, Optional, cast
 
-from cuenca_validations.types import Address, CurpField, Rfc
+from cuenca_validations.types import Address, CurpField, KYCUpdateRequest, Rfc
 from pydantic.dataclasses import dataclass
 
 from ..http import Session, session as global_session
-from .base import Creatable, Retrievable
+from .base import Creatable, Retrievable, Updateable
 
 
 @dataclass
-class KYCVerification(Creatable, Retrievable):
+class KYCVerification(Creatable, Retrievable, Updateable):
     _resource: ClassVar = 'kyc_verifications'
 
     platform_id: str
@@ -23,3 +23,18 @@ class KYCVerification(Creatable, Retrievable):
     @classmethod
     def create(cls, session: Session = global_session) -> 'KYCVerification':
         return cast('KYCVerification', cls._create(session=session))
+
+    @classmethod
+    def update(
+        cls,
+        kyc_id: str,
+        curp: Optional[CurpField] = None,
+        rfc: Optional[Rfc] = None,
+    ) -> 'KYCVerification':
+        request = KYCUpdateRequest(
+            curp=curp,
+            rfc=rfc,
+        )
+        return cast(
+            'KYCVerification', cls._update(id=kyc_id, **request.dict())
+        )
--- tests/resources/test_kyc_verifications.py
+++ tests/resources/test_kyc_verifications.py
@@ -11,5 +11,17 @@ def test_kyc_verification_create():
 
 @pytest.mark.vcr
 def test_kyc_verification_retrieve():
-    kyc_verification: KYCVerification = KYCVerification.retrieve('KYC01')
+    kyc_verification = KYCVerification.retrieve('KYC01')
     assert kyc_verification.id
+
+
+@pytest.mark.vcr
+def test_kyc_verification_update():
+    kyc_id = 'KYC01'
+    changes = dict(
+        curp='HEMA921130HNERNN05',
+    )
+    kyc_verification = KYCVerification.update(kyc_id, **changes)
+    assert all(
+        item in kyc_verification.to_dict().items() for item in changes.items()
+    )
