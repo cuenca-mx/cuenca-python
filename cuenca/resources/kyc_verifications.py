963d1909c9d43a4e7f2a358b3d4bbe351e589b57
Yisus <alan@cuenca.com>
2022-05-12 13:05:54 -0500

    Update kyc curp (#265)
    * Update kyc curp
    
    * comments
    
    * bump version

--- cuenca/resources/kyc_verifications.py
+++ cuenca/resources/kyc_verifications.py
@@ -1,15 +1,20 @@
 import datetime as dt
 from typing import ClassVar, Optional, cast
 
-from cuenca_validations.types import Address, CurpField, Rfc
+from cuenca_validations.types import (
+    Address,
+    CurpField,
+    KYCVerificationUpdateRequest,
+    Rfc,
+)
 from pydantic.dataclasses import dataclass
 
 from ..http import Session, session as global_session
-from .base import Creatable, Retrievable
+from .base import Creatable, Retrievable, Updateable
 
 
 @dataclass
-class KYCVerification(Creatable, Retrievable):
+class KYCVerification(Creatable, Retrievable, Updateable):
     _resource: ClassVar = 'kyc_verifications'
 
     platform_id: str
@@ -23,3 +28,12 @@ class KYCVerification(Creatable, Retrievable):
     @classmethod
     def create(cls, session: Session = global_session) -> 'KYCVerification':
         return cast('KYCVerification', cls._create(session=session))
+
+    @classmethod
+    def update(
+        cls,
+        kyc_id: str,
+        curp: Optional[CurpField] = None,
+    ) -> 'KYCVerification':
+        req = KYCVerificationUpdateRequest(curp=curp)
+        return cast('KYCVerification', cls._update(id=kyc_id, **req.dict()))
