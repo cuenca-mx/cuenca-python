f5c06dc2d242722b15041d93fd294235a2b29862
rogeliolopez <rogelio.lpz94@gmail.com>
2022-05-27 13:23:30 -0500

    docs in config

--- cuenca/resources/curp_validations.py
+++ cuenca/resources/curp_validations.py
@@ -31,6 +31,48 @@ class CurpValidation(Creatable, Retrievable):
     renapo_curp_match: bool
     renapo_full_match: bool
 
+    class Config:
+        fields = {
+            'names': {'description': 'Official name from Renapo'},
+            'first_surname': {'description': 'Official surname from Renapo'},
+            'second_surname': {'description': 'Official surname from Renapo'},
+            'state_of_birth': {'description': 'In format ISO 3166 Alpha-2'},
+            'nationality': {'description': 'In format ISO 3166 Alpha-2'},
+            'manual_curp': {'description': 'curp provided in request'},
+            'calculated_curp': {
+                'description': 'Calculated curp based on request data'
+            },
+            'validated_curp': {
+                'description': 'Curp validated in Renapo, null if not exists'
+            },
+            'renapo_curp_match': {
+                'description': 'True if curp exists and is valid'
+            },
+            'renapo_full_match': {
+                'description': 'True if all fields provided match the response from RENAPO. '
+                'Accents in names are ignored'
+            },
+        }
+        schema_extra = {
+            "example": {
+                "id": "CV-123",
+                "created_at": "2019-08-24T14:15:22Z",
+                "names": "Guillermo",
+                "first_surname": "Gonzales",
+                "second_surname": "Camarena",
+                "date_of_birth": "1965-04-18",
+                "country_of_birth": "MX",
+                "state_of_birth": "VZ",
+                "gender": "male",
+                "nationality": "MX",
+                "manual_curp": None,
+                "calculated_curp": "GOCG650418HVZNML08",
+                "validated_curp": "GOCG650418HVZNML08",
+                "renapo_curp_match": True,
+                "renapo_full_match": True,
+            }
+        }
+
     @classmethod
     def create(
         cls,
