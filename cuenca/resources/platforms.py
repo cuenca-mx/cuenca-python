45723827f96690c0ddadc53536199143792b2b51
Manuel Avalos <manuelavalos97@gmail.com>
2022-06-06 12:12:56 -0500

    More fields for platforms

--- cuenca/resources/platforms.py
+++ cuenca/resources/platforms.py
@@ -1,7 +1,7 @@
 import datetime as dt
-from typing import ClassVar, cast
+from typing import ClassVar, Optional, cast
 
-from cuenca_validations.types import PlatformRequest
+from cuenca_validations.types import Country, PlatformRequest, State
 
 from ..http import Session, session as global_session
 from .base import Creatable
@@ -12,16 +12,35 @@ class Platform(Creatable):
 
     created_at: dt.datetime
     name: str
+    rfc_curp: Optional[str] = None
+    establishment_date: Optional[str] = None
+    country: Optional[Country] = None
+    state: Optional[State] = None
+    economic_activity: Optional[str] = None
 
     class Config:
         fields = {
-            'name': {'description': 'name of the platform being created'}
+            'name': {'description': 'name of the platform being created'},
+            'rfc_curp': {'description': 'name of the platform being created'},
+            'establishment_date': {
+                'description': 'name of the platform being created'
+            },
+            'country': {'description': 'name of the platform being created'},
+            'state': {'description': 'name of the platform being created'},
+            'economic_activity': {
+                'description': 'name of the platform being created'
+            },
         }
         schema_extra = {
             'example': {
                 'id': 'PT0123456789',
                 'name': 'Arteria',
                 'created_at': '2021-08-24T14:15:22Z',
+                'rfc_curp': 'ART123456FFF',
+                'establishment_date': '2021-08-24T14:15:22Z',
+                'country': 'MX',
+                'state': 'DF',
+                'economic_activity': 'fiinances and technologies',
             }
         }
 
