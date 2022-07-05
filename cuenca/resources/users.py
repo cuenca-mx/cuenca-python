551e7b9bcc167a1bd83104efbd9ad7cf1844d020
Yisus <alan@cuenca.com>
2022-07-05 14:36:59 -0500

    remove breakpoint

--- cuenca/resources/users.py
+++ cuenca/resources/users.py
@@ -145,7 +145,6 @@ class User(Creatable, Retrievable, Updateable, Queryable):
         *,
         session: Session = global_session,
     ):
-        # breakpoint()
         request = UserUpdateRequest(
             phone_number=phone_number,
             email_address=email_address,
