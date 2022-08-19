8645ede40d217970c7280c0ebe3ad7302c09fe00
pachCode <arturo@cuenca.com>
2022-08-18 22:16:07 -0500

    Add clabe resource

---
+++ cuenca/resources/clabes.py
@@ -0,0 +1,17 @@
+import datetime as dt
+from typing import ClassVar, cast
+
+from ..http import Session, session as global_session
+from .base import Creatable, Retrievable
+
+
+class Clabes(Creatable, Retrievable):
+    _resource: ClassVar = 'clabes'
+    clabe: str
+    created_at: dt.datetime
+    user_id: str
+
+    @classmethod
+    def create(cls, user_id: str = 'me', session: Session = global_session):
+        req = dict(user_id=user_id)
+        return cast('Clabes', cls._create(session=session, **req))
