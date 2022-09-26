aeebddf2061030c89f1c649e46605ef31926be31
AlexViquez <alexviquez22@gmail.com>
2022-09-26 18:51:41 -0500

    fix test

--- cuenca/resources/kyc_validations.py
+++ cuenca/resources/kyc_validations.py
@@ -1,7 +1,6 @@
-import datetime as dt
 from typing import ClassVar, List, Optional, cast
 
-from cuenca_validations.types import KYCFile, KYCValidationRequest
+from cuenca_validations.types import KYCValidationRequest
 
 from ..http import Session, session as global_session
 from .base import Creatable, Queryable, Retrievable
@@ -21,7 +20,7 @@ class KYCValidation(Creatable, Retrievable, Queryable):
                 'platform_id': 'PT8UEv02zBTcymd4Kd3MO6pg',
                 'created_at': '2020-05-24T14:15:22Z',
                 'verification_id': 'string',
-                'files_uri': ['cuenca.com/files/id', 'cuenca.com/files/id2']
+                'files_uri': ['cuenca.com/files/id', 'cuenca.com/files/id2'],
                 'attemps': '1',
             }
         }
