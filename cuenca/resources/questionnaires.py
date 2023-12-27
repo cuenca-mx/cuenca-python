e9675151aa8cd153e04d3739cb8c0f795b494fc1
AlexViquez <alexviquez22@gmail.com>
2023-12-26 19:34:52 -0600

    questionnare first version

---
+++ cuenca/resources/questionnaires.py
@@ -0,0 +1,40 @@
+import datetime as dt
+from typing import ClassVar, cast
+
+from cuenca_validations.types import QuestionnaireRequest
+
+from ..http import Session, session as global_session
+from .base import Creatable, Retrievable
+
+
+class Questionnaires(Creatable, Retrievable):
+    _resource: ClassVar = 'questionnaires'
+
+    created_at: dt.datetime
+
+    class Config:
+        schema_extra = {
+            'example': {
+                'user_id': 'US234i23jh23h4h23',
+                'token': '3223j23ij23ij3',
+                'alert_id': 'ALewifjwiejf',
+            }
+        }
+
+    @classmethod
+    def create(
+        cls,
+        user_id: str,
+        token: str,
+        alert_id: str,
+        *,
+        session: Session = global_session,
+    ) -> 'Questionnaires':
+        req = QuestionnaireRequest(
+            user_id=user_id,
+            token=token,
+            alert_id=alert_id,
+        )
+        return cast(
+            'Questionnaires', cls._create(session=session, **req.dict())
+        )
