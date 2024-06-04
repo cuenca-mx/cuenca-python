8970e148ebebf7874048d63d9f3ef8b3c7e97b6e
pachCode <arturo@cuenca.com>
2024-06-04 13:31:32 -0600

    Add test

---
+++ tests/resources/test_otps.py
@@ -0,0 +1,28 @@
+import pytest
+
+from cuenca import LoginToken, Otp, UserLogin
+from cuenca.http.client import Session
+
+
+@pytest.fixture(scope='function')
+def session():
+    session = Session()
+    session.configure(
+        'api_key',
+        'api_secret',
+        sandbox=True,
+    )
+    return session
+
+
+@pytest.mark.vcr
+def test_otps(session):
+    UserLogin.create('111111', session=session)
+    login_token = LoginToken.create(session=session)
+    session.headers.pop(
+        'X-Cuenca-LoginId',
+    )
+    session.configure(login_token=login_token.id)
+    otp = Otp.create()
+    assert otp
+    assert type(otp.secret) == str
