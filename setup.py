52f9e2086ddac615a2922c3aedd76705d4d9f0f7
gabino <gabino@cuenca.com>
2025-01-16 16:51:14 -0600

    chore: update dependencies in requirements and setup files

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ setup(
     package_data=dict(cuenca=['py.typed']),
     python_requires='>=3.9',
     install_requires=[
-        'requests>=2.32.3',
-        'cuenca-validations==2.0.0.dev13',
-        'pydantic-extra-types>=2.10.1',
+        'requests>=2.32.0',
+        'cuenca-validations>=2.0.0',
+        'pydantic-extra-types>=2.10.0',
     ],
     classifiers=[
         "Programming Language :: Python :: 3.9",
