1a1d12e4a9b45bf3b0386f5143fc20380e890772
gabino <gabino@cuenca.com>
2025-01-30 16:22:59 -0600

    Simplify JwtToken test by removing session configuration

--- tests/resources/test_jwt_tokens.py
+++ tests/resources/test_jwt_tokens.py
@@ -1,28 +1,10 @@
 import pytest
 
-from cuenca import JwtToken, LoginToken, UserLogin
-from cuenca.http.client import Session
-
-
-@pytest.fixture(scope='function')
-def session():
-    session = Session()
-    session.configure(
-        'api_key',
-        'api_secret',
-        sandbox=True,
-    )
-    return session
+from cuenca import JwtToken
 
 
 @pytest.mark.vcr
-def test_jwt_tokens(session):
-    UserLogin.create('111111', session=session)
-    login_token = LoginToken.create(session=session)
-    session.headers.pop(
-        'X-Cuenca-LoginId',
-    )
-    session.configure(login_token=login_token.id)
-    jwt_token = JwtToken.create(session=session)
+def test_jwt_tokens():
+    jwt_token = JwtToken.create()
     assert jwt_token
     assert isinstance(jwt_token.token, str)
