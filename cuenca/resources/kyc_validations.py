08c40144d8ba17d442c25a519542d17f4e867763
rogeliolopez <rogelio.lpz94@gmail.com>
2025-03-27 15:28:39 -0600

    fields

--- cuenca/resources/kyc_validations.py
+++ cuenca/resources/kyc_validations.py
@@ -10,19 +10,25 @@ from .base import Creatable, Queryable, Retrievable
 class KYCValidation(Creatable, Retrievable, Queryable):
     _resource: ClassVar = 'kyc_validations'
     platform_id: str
-    attemps: Optional[int] = None
-    verification_id: Optional[str] = None
+    user_id: str
+    source_type: KYCValidationSource
+    flow_id: str
     files_uri: Optional[list[str]] = None
+    verification_id: Optional[str] = None
+    identity_id: Optional[str] = None
 
     model_config = ConfigDict(
         json_schema_extra={
             'example': {
                 'id': 'KVNEUInh69SuKXXmK95sROwQ',
-                'platform_id': 'PT8UEv02zBTcymd4Kd3MO6pg',
                 'created_at': '2020-05-24T14:15:22Z',
-                'verification_id': 'string',
+                'platform_id': 'PT-1234567890',
+                'user_id': 'US-1234567890',
+                'source_type': 'server',
+                'flow_id': '123e4567-e89b-12d3-a456-426614174000',
                 'files_uri': ['cuenca.com/files/id', 'cuenca.com/files/id2'],
-                'attemps': '1',
+                'verification_id': 'metamap-verification-id',
+                'identity_id': 'metamap-identity-id',
             }
         }
     )
