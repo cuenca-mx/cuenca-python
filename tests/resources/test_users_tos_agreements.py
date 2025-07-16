62369a914b1e7631c79a215719e0ea4e69b15b77
gabino <gabino.morales@outlook.com>
2025-07-16 17:25:48 -0600

    Update test_user_tos_agreements_create function signature to include return type annotation

--- tests/resources/test_users_tos_agreements.py
+++ tests/resources/test_users_tos_agreements.py
@@ -4,7 +4,7 @@ from cuenca.resources import UserTOSAgreement
 
 
 @pytest.mark.vcr
-def test_user_tos_agreements_create():
+def test_user_tos_agreements_create() -> None:
     tos = UserTOSAgreement.create(
         location="9.3953792,-99.139584",
         tos_id="TS67f5bf03c1fc891bdf36090d",
