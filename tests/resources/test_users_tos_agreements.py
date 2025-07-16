7f95e7744e1f2a654049fb5497dcc532fc2d1797
gabino <gabino.morales@outlook.com>
2025-07-16 17:10:44 -0600

    Update test_user_tos_agreements to use string format for location coordinates

--- tests/resources/test_users_tos_agreements.py
+++ tests/resources/test_users_tos_agreements.py
@@ -6,7 +6,7 @@ from cuenca.resources import UserTOSAgreement
 @pytest.mark.vcr
 def test_user_tos_agreements_create():
     tos = UserTOSAgreement.create(
-        location=dict(latitude=9.3953792, longitude=-99.139584),
+        location="9.3953792,-99.139584",
         tos_id="TS67f5bf03c1fc891bdf36090d",
     )
     assert tos.id
