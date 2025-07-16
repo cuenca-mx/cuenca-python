015c6bc9bcb056b5a4394190bc4624465065b16e
gabino <gabino.morales@outlook.com>
2025-07-16 15:45:37 -0600

    Add create class method for instantiating UserTOSAgreement with a signature image URL

--- cuenca/resources/users_tos_agreements.py
+++ cuenca/resources/users_tos_agreements.py
@@ -1,8 +1,13 @@
 import datetime as dt
-from typing import ClassVar
+from typing import ClassVar, Optional
 
 from cuenca_validations.types.general import SerializableHttpUrl
+from cuenca_validations.types.requests import (
+    FileCuencaUrl,
+    UserTOSAgreementRequest,
+)
 
+from ..http import Session, session as global_session
 from .base import Creatable, Queryable, Retrievable
 
 
@@ -12,10 +17,27 @@ class UserTOSAgreement(Creatable, Retrievable, Queryable):
     id: str
     created_at: dt.datetime
     updated_at: dt.datetime
-    terms_of_service: str
+    terms_of_service_uri: str
     user_id: str
     ip: str
     location: str
-    digital_signature: str
-    signed_document_url: SerializableHttpUrl
-    notification_id: str
+    digital_signature: Optional[str] = None
+    signature_image_url: Optional[FileCuencaUrl] = None
+    signed_document_url: Optional[SerializableHttpUrl] = None
+    notification_id: Optional[str] = None
+
+    @classmethod
+    def create(
+        cls,
+        location: str,
+        tos_id: str,
+        signature_image_url: Optional[FileCuencaUrl] = None,
+        *,
+        session: Session = global_session,
+    ) -> 'UserTOSAgreement':
+        req = UserTOSAgreementRequest(
+            location=location,
+            tos_id=tos_id,
+            signature_image_url=signature_image_url,
+        )
+        return cls._create(session=session, **req.model_dump())
---
+++ tests/resources/test_users_tos_agreements.py
@@ -0,0 +1,12 @@
+import pytest
+
+from cuenca.resources import UserTOSAgreement
+
+
+@pytest.mark.vcr
+def test_user_tos_agreements_create():
+    tos = UserTOSAgreement.create(
+        location=dict(latitude=9.3953792, longitude=-99.139584),
+        tos_id="TS67f5bf03c1fc891bdf36090d",
+    )
+    assert tos.id
